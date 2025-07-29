67bc10683ab3504516877dd68dbd4c3eb78f5cfb
Ryan <ryangadinga90@gmail.com>
2025-07-29 09:37:33 +0700

    add package-lock.json, update my-element.js

--- src/js/components/my-element.js
+++ src/js/components/my-element.js
@@ -1,5 +1,24 @@
-import { LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 
-class MyElement extends LitElement {}
+class MyElement extends LitElement {
+  static properties = {};
+
+  static styles = css`
+    :host {
+      display: block;
+    }
+  `;
+
+  render() {
+    return html`
+      <p>
+        <input type="color" />
+      </p>
+
+      <button>Terapkan</button>
+      <button>Warna Acak</button>
+    `;
+  }
+}
 
 customElements.define('my-element', MyElement);
